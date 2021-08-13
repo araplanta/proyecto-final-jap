df3589147ac15d30d0c19876916889cae5aec090
Araceli Laluz <aracelilaluz@gmail.com>
2021-08-13 18:27:23 -0300

    entrega1(inProgress)
    Cambios realizados para la entrega 1. Falta terminar y mejorar.

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+const loginForm = document.getElementById("login-form");
 
+document.addEventListener("DOMContentLoaded", function(e){
+    const username = loginForm.login.value;
+    const password = loginForm.password.value;
+    if (username === "prueba@prueba.com" && password === "prueba") {
+        alert("WE DID IT!!!");
+        location.reload();
+    } else {
+        alert("Correo electrónico y/o contraseña incorrecta, por favor vuelve a intentarlo");
+    }
 });
