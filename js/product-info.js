7b58e1c5108de56fd930c1fbeb2279fe859123bb
Araceli Laluz <aracelilaluz@gmail.com>
2021-09-19 23:43:01 -0300

    entrega3-finalResult

--- js/product-info.js
+++ js/product-info.js
@@ -1,8 +1,9 @@
 var product = {};
-function showImages(array){
+//Función que muestra las imágenes del producto de forma ordenada
+function showImages(array) {
     let htmlContentToAppend = "";
 
-    for(let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         let imageSrc = array[i];
 
         htmlContentToAppend += `
@@ -17,45 +18,74 @@ function showImages(array){
     }
 }
 
-function showProductComments(){
+//Función que muestra los comentarios ya existentes del producto
+function showProductComments() {
     let htmlContentToAppend = "";
     let estrellita = `<span class="fa fa-star checked"></span>`;
     let noEstrellita = `<span class="fa fa-star"></span>`;
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            for (const comment of resultObj.data ) {
-            htmlContentToAppend += `
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            for (const comment of resultObj.data) {
+                htmlContentToAppend += `
             <div class="media-block">
             <div class="media-body">
             <div class="mar-btm">
-                <p class="text-muted text-sm">`+ `<a href="#" class="btn-link text-semibold media-heading box-inline">`+ comment.user +`</a> `+ comment.dateTime + `</p>
+                <p class="text-muted text-sm">`+ `<a href="" class="btn-link text-semibold media-heading box-inline">` + comment.user + `</a> ` + comment.dateTime + `</p>
            
              </div>
-            <p>`+ comment.description + ` `+ estrellita.repeat(comment.score)+ noEstrellita.repeat(5- comment.score) + `</p>
+            <p>`+ comment.description + ` ` + estrellita.repeat(comment.score) + noEstrellita.repeat(5 - comment.score) + `</p>
             </div>
             </div>
             `
             }
             document.getElementById("commentsPublicados").innerHTML = htmlContentToAppend;
-        }      
+        }
     });
 }
+
+//Función para cargar un nuevo comentario a la página del producto
+function newComment() {
+    let htmlAgregar = "";
+    let comentario = document.getElementById("comment").value;
+    let user = localStorage.getItem("user");
+    let estrellita = `<span class="fa fa-star checked"></span>`;
+    let noEstrellita = `<span class="fa fa-star"></span>`;
+    let score = document.getElementById("stars").value;
+    let date = new Date();
+    if (comentario == "") {
+        alert("Porfavor introduce la informacion requerida!");
+    } else {
+        htmlAgregar += `
+        <div class="media-block">
+            <div class="media-body">
+            <div class="mar-btm">
+                <p class="text-muted text-sm">`+ `<a href="" class="btn-link text-semibold media-heading box-inline">` + user + `</a> ` + date.getFullYear() + `-` + date.getMonth() + `-` + date.getDate() + ` ` + date.getHours() + `:` + date.getMinutes() + `:` + date.getSeconds() + `</p>
+             </div>
+            <p>`+ comentario + ` ` + estrellita.repeat(score) + noEstrellita.repeat(5 - score) + `</p>
+            </div>
+            </div>
+            `
+    }
+    document.getElementById("commentsPublicados").innerHTML += htmlAgregar;
+}
+
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
             product = resultObj.data;
 
-            let productNameHTML  = document.getElementById("productName");
+            // Se carga la información del producto dentro del HTML
+            let productNameHTML = document.getElementById("productName");
             let productPriceHTML = document.getElementById("productPrice")
             let productDescriptionHTML = document.getElementById("productDescription");
             let productCategoryHTML = document.getElementById("productCategory")
             let productCountHTML = document.getElementById("productCount");
-        
+
             productNameHTML.innerHTML = product.name;
             productPriceHTML.innerHTML = product.cost + " " + product.currency;
             productDescriptionHTML.innerHTML = product.description;
@@ -64,7 +94,12 @@ document.addEventListener("DOMContentLoaded", function(e){
 
             //Muestro las imagenes en forma de galería
             showImages(product.images);
+            //Muestro los comentarios ya existentes
             showProductComments();
         }
     });
+});
+//Muestro el nuevo comentario
+document.getElementById("enviarComment").addEventListener("click", function () {
+    newComment();
 });
