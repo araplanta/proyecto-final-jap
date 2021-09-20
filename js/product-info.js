234a13f1190dfff9e34e7a5a7ee079fe18e144a4
Araceli Laluz <aracelilaluz@gmail.com>
2021-09-19 22:35:54 -0300

    entrega3-intento2

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,4 @@
 var product = {};
-
 function showImages(array){
     let htmlContentToAppend = "";
 
@@ -18,6 +17,30 @@ function showImages(array){
     }
 }
 
+function showProductComments(){
+    let htmlContentToAppend = "";
+    let estrellita = `<span class="fa fa-star checked"></span>`;
+    let noEstrellita = `<span class="fa fa-star"></span>`;
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            for (const comment of resultObj.data ) {
+            htmlContentToAppend += `
+            <div class="media-block">
+            <div class="media-body">
+            <div class="mar-btm">
+                <p class="text-muted text-sm">`+ `<a href="#" class="btn-link text-semibold media-heading box-inline">`+ comment.user +`</a> `+ comment.dateTime + `</p>
+           
+             </div>
+            <p>`+ comment.description + ` `+ estrellita.repeat(comment.score)+ noEstrellita.repeat(5- comment.score) + `</p>
+            </div>
+            </div>
+            `
+            }
+            document.getElementById("commentsPublicados").innerHTML = htmlContentToAppend;
+        }      
+    });
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -32,17 +55,16 @@ document.addEventListener("DOMContentLoaded", function(e){
             let productDescriptionHTML = document.getElementById("productDescription");
             let productCategoryHTML = document.getElementById("productCategory")
             let productCountHTML = document.getElementById("productCount");
-            let productRelatedHTML = document.getElementById("productRelated");
         
             productNameHTML.innerHTML = product.name;
             productPriceHTML.innerHTML = product.cost + " " + product.currency;
             productDescriptionHTML.innerHTML = product.description;
             productCategoryHTML.innerHTML = product.category;
             productCountHTML.innerHTML = product.soldCount;
-            productRelatedHTML.innerHTML = product.relatedProducts;
 
             //Muestro las imagenes en forma de galería
             showImages(product.images);
+            showProductComments();
         }
     });
 });
