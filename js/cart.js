c27658a353e0912590a74ba9a6266f5d64fbda34
Araceli Laluz <aracelilaluz@gmail.com>
2021-10-17 23:11:30 -0300

    entrega5-sinComments

--- js/cart.js
+++ js/cart.js
@@ -1,29 +1,100 @@
+let cartProductsArray;
+let shippingPer = 0;
 //Función para mostrar la información de carrito dentro de la tabla de productos
 function showCartProducts() {
     let htmlContentToAppend = "";
+    document.getElementById("cartProds").innerHTML = htmlContentToAppend;
     for (let i = 0; i < cartProductsArray.articles.length; i++) {
         let product = cartProductsArray.articles[i];
         htmlContentToAppend += `
-        <tr>
+        <tr id="prod `+ i +`">
         <td><img src="` + product.src + `" alt="` + product.name + `" width="35px"></td>
         <td>`+ product.name + `</td>
         <td>
             <div class="form-group mb-0">
-              <input class="form-control" style="width:65px;" type="number" id="productCount" value="`+ product.count + `" min="1">
+              <input class="form-control quantity" style="width:65px;" type="number" id="productCount-`+ i +`" value="`+ product.count + `" min="1">
             </div>
         </td>
-        <td>`+ product.currency + ` ` + product.unitCost + `</td>
-        <td class="text-right">pum</td>
+        <td>`+ product.currency + ` ` + product.count * product.unitCost + `</td>
+        <td class="text-right" id="subtotal"></td>
         <td><a href="#" class="text-danger"><i class="ri-delete-bin-3-line"></i></a></td>
         </tr>
         `
+        document.getElementById("cartProds").innerHTML += htmlContentToAppend;
     }
-    document.getElementById("cartProds").innerHTML += htmlContentToAppend;
+    
+    var quantityInputs =  document.getElementsByClassName('quantity');
+    for (let i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener('change',quantityChange);
+    }
+
+    getSubtotal()
 }
 
-/*function subtotalCost(){
+function shippingChange(shipping){
+    shippingPer = shipping.value;
+    showCartProducts()
+}
 
-}*/
+function ready(){
+    var quantityInputs =  document.getElementsByClassName('quantity');
+    for (let i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener('change',quantityChange);
+        
+    }
+}
+
+function quantityChange(event){
+    var input = event.target;
+    if(isNaN(input.value) || input.value <= 0){
+        alert("el valor no puede ser menor a 1")
+        input.value = 1
+    }
+    
+    // se encarga de dividir la id en 2 basado en el caracter -
+    splitedID = event.target.id.split("-")
+    // se utiliza la posocion 1 ya que la 0 solo tiene la string generica
+    productNumberChange = splitedID[1]
+    // se modifica el count del articulo basado en el valor del input
+    cartProductsArray.articles[productNumberChange].count = input.value
+    showCartProducts()
+    
+
+}
+function getSubtotal(){
+    let htmlContentToAppend = "";
+    document.getElementById("total").innerHTML = htmlContentToAppend;
+    let totalProducts = 0;
+    for (let i = 0; i < cartProductsArray.articles.length; i++) {
+        let product = cartProductsArray.articles[i];
+        var unitCost = product.unitCost;
+        var quantity = product.count;
+        totalProducts += (unitCost * quantity)
+    }
+
+    envio = 0;
+    if (shippingPer != 0) {
+        envio  = (totalProducts * shippingPer) / 100;
+    }
+    
+    total = totalProducts + envio;
+    htmlContentToAppend = `
+    <tr>
+        <td>Total (productos) :</td>
+        <td>$`+totalProducts+`</td>
+    </tr>
+    <tr>
+        <td>Envío :</td>
+        <td>$`+envio+`</td>
+    </tr>
+    <tr>
+        <td class="f-w-7 font-18"><h4>Total :</h4></td>
+        <td class="f-w-7 font-18"><h4>$`+total+`</h4></td>
+    </tr>`;
+    document.getElementById("total").innerHTML = htmlContentToAppend;
+}
 
 
 
@@ -35,7 +106,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
         if (resultObj.status === "ok") {
             cartProductsArray = resultObj.data;
             //Muestra los productos dentro del carrito
-            showCartProducts(cartProductsArray);
+            showCartProducts();
         }
     });
 });
+
